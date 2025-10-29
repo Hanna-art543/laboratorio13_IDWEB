b442bece5a6c6039d33a0918b4e8d4d2d8da560f
Ana Miranda <amirandalo@unsa.edu.pe>
2025-10-29 14:47:26 -0500

    Subir ejercicio02.js

---
+++ ejercicios/ejercicio02.js
@@ -0,0 +1,19 @@
+//Función filtrar y transformar
+function filtrarYMostrar(arreglo) {
+    
+    let positivos = arreglo.filter(num => num >= 0);
+    let cuadrados = positivos.map (x => Math.pow(x, 2));
+
+    //Suma total
+    let sumaTotal = 0;
+    for (let elemento of cuadrados) {
+        sumaTotal += elemento;
+    }
+
+    return sumaTotal;
+}
+
+//Ingresar y mostrar información
+let arreglo = [-2, 3, -5, 4];
+alert("Arreglo: "+arreglo+"\nSuma total (postivos al cuadrado): "+filtrarYMostrar(arreglo));
+
